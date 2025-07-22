bab829203752a8a919cdac1e0e8371adc681f081
sangjun9786 <breamzzang@naver.com>
2025-07-22 13:06:28 +0900

    7/22 테스트

---
+++ src/main/java/com/btw09/buyyourbrain/AGosuApplication.java
@@ -0,0 +1,14 @@
+//package com.btw09.buyyourbrain;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class AGosuApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(AGosuApplication.class, args);
+//	}
+//
+//}
+//test
